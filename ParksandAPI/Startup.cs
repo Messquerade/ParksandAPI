506fd922fa211d2b22585c3789fc7542970d3387
Messquerade <annac.klingberg@gmail.com>
2021-10-29 11:11:14 -0700

    add Swagger

--- ParksandAPI/Startup.cs
+++ ParksandAPI/Startup.cs
@@ -23,12 +23,14 @@ namespace ParksandAPI
             services.AddDbContext<ParksandAPIContext>(opt =>
                 opt.UseMySql(Configuration["ConnectionStrings:DefaultConnection"], ServerVersion.AutoDetect(Configuration["ConnectionStrings:DefaultConnection"])));
             services.AddControllers();
-            
+            services.AddSwaggerGen();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
-        {
+        {   
+            app.UseSwagger();
+            app.UseSwaggerUI(c => {c.RoutePrefix = "swagger"; c.SwaggerEndpoint("/swagger/v1/swagger.json", "ParksandAPI v1");} );
             if (env.IsDevelopment())
             {
                 app.UseDeveloperExceptionPage();
