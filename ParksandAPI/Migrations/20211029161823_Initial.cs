6ff75e616314ea3a00bc858b9cb8456ef6440d58
Messquerade <annac.klingberg@gmail.com>
2021-10-29 10:27:16 -0700

    add ParksController, routes

---
+++ ParksandAPI/Migrations/20211029161823_Initial.cs
@@ -0,0 +1,35 @@
+﻿using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace ParksandAPI.Migrations
+{
+    public partial class Initial : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Parks",
+                columns: table => new
+                {
+                    ParkId = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("MySql:ValueGenerationStrategy", MySqlValueGenerationStrategy.IdentityColumn),
+                    Name = table.Column<string>(type: "longtext CHARACTER SET utf8mb4", nullable: true),
+                    StatePark = table.Column<bool>(type: "tinyint(1)", nullable: false),
+                    NationalPark = table.Column<bool>(type: "tinyint(1)", nullable: false),
+                    Activities = table.Column<string>(type: "longtext CHARACTER SET utf8mb4", nullable: true),
+                    Acreage = table.Column<int>(type: "int", nullable: false),
+                    Address = table.Column<string>(type: "longtext CHARACTER SET utf8mb4", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Parks", x => x.ParkId);
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Parks");
+        }
+    }
+}
